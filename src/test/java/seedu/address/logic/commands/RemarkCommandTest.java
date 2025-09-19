65b54e3c0a3ed3df3bc867c00b1e47681ad715d6
Eugene Oh Yun Zheng <eugeneoyz.12.03.n@gmail.com>
2025-09-19 18:27:18 +0800

    Fix to match code standards

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -1,9 +1,9 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-
 import org.junit.jupiter.api.Test;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.RemarkCommand.MESSAGE_NOT_IMPLEMENTED_YET;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 
@@ -13,6 +13,6 @@ public class RemarkCommandTest {
 
     @Test
     public void execute() {
-        assertCommandFailure(new RemarkCommand(), model, "");
+        assertCommandFailure(new RemarkCommand(), model, MESSAGE_NOT_IMPLEMENTED_YET);
     }
 }
